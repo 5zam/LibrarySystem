1eb434a5485df4f48e1376c138cd94d224a30e36
5zam <zuwina.s.aljabri@gmail.com>
2023-10-22 23:37:58 +0300

    update2

--- src/main/java/com/example/LibraryManagementSystem/Library/Entities/Book.java
+++ src/main/java/com/example/LibraryManagementSystem/Library/Entities/Book.java
@@ -1,4 +1,62 @@
 package com.example.LibraryManagementSystem.Library.Entities;
 
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Objects;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class Book {
+
+    @ManyToOne
+    @JoinColumn(name = "author_id")
+    private Author author;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id; // You can use a database-generated ID
+
+    private String isbn;
+    private String title;
+    private String category;
+    private int quantity;
+
+    public Book(String isbn, String title, String category, int quantity) {
+        this.isbn = isbn;
+        this.title = title;
+        this.category = category;
+        this.quantity = quantity;
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return quantity == book.quantity && Objects.equals(id, book.id) && Objects.equals(isbn, book.isbn) && Objects.equals(title, book.title) && Objects.equals(category, book.category);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, isbn, title, category, quantity);
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", isbn='" + isbn + '\'' +
+                ", title='" + title + '\'' +
+                ", category='" + category + '\'' +
+                ", quantity=" + quantity +
+                '}';
+    }
+
 }
